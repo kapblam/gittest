477075a23fd7755656ff0e0cbb5c092a051ff8c7
kapblam <nik.kaplan@gmail.com>
2018-07-15 20:34:48 -0700

    first commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    //int x = 5;
+    cout << "Enter a value for x: ";
+    int x;
+    cin >> x;
+    cout << "the value entered is: " << x << endl;
+    x = x-2;
+    cout << "x = " << x << endl;
+
+    int y = x;
+    cout << y << endl;
+
+    cout << x+y << endl;
+    cout << "Allie loves Nik?" << x << endl;
+    cout << "Yes, she does!" << endl;
+
+    int z = 0;
+    cout << z << endl;
+
+    return 0;
+    }
