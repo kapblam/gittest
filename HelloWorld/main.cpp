a4211803b982eade7176934be10adce380282fe7
kapblam <nik.kaplan@gmail.com>
2018-07-15 21:33:59 -0700

    rev2

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
+#include "add.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-
-    //int x = 5;
-    cout << "Enter a value for x: ";
-    int x;
-    cin >> x;
-    cout << "the value entered is: " << x << endl;
-    x = x-2;
-    cout << "x = " << x << endl;
-
-    int y = x;
-    cout << y << endl;
-
-    cout << x+y << endl;
-    cout << "Allie loves Nik?" << x << endl;
-    cout << "Yes, she does!" << endl;
-    cout << "Nik likes sparkling water, Yaaaaasssss!" << endl;
-
-    int z = 0;
-    cout << z << endl;
-
+    cout << "The sum of 3 and 4 is: " << add(3,4) << endl;
     return 0;
-    }
+}
